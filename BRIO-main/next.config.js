a1c0dced18fcab8cc224d60c6fc7212a823359d1
Rida Sabir <ridamalik7113@gmail.com>
2026-06-30 06:35:42 +0500

    Restore widget page

--- BRIO-main/next.config.js
+++ BRIO-main/next.config.js
@@ -1,23 +1,49 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/widget",
-        destination: "/widget/",
-      },
-    ];
-  },
-
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "images.unsplash.com", port: "", pathname: "/**" },
-      { protocol: "https", hostname: "seo-heist.s3.amazonaws.com", port: "", pathname: "/**" },
-      { protocol: "https", hostname: "github.com", port: "", pathname: "/**" },
-      { protocol: "https", hostname: "ansubkhan.com", port: "", pathname: "/**" },
-      { protocol: "https", hostname: "utfs.io", port: "", pathname: "/**" },
-      { protocol: "https", hostname: "lh3.googleusercontent.com", port: "", pathname: "/**" },
-      { protocol: "https", hostname: "zova.s3.us-east-2.amazonaws.com", port: "", pathname: "/**" },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "seo-heist.s3.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "github.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "ansubkhan.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "zova.s3.us-east-2.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 
