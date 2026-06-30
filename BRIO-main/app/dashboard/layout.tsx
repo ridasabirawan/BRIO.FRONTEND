886a19e6df16d914d1e4a61ee0359bc05ad1dc60
Rida Sabir <ridamalik7113@gmail.com>
2026-06-30 18:39:21 +0500

    Update landing and dashboard backgrounds

--- BRIO-main/app/dashboard/layout.tsx
+++ BRIO-main/app/dashboard/layout.tsx
@@ -4,6 +4,7 @@ import DashboardTopNav from "./_components/dashbord-top-nav";
 import { auth } from "@/utils/auth";
 import { redirect } from "next/navigation";
 import WelcomePopup from "@/components/welcome-popup";
+import AnimatedBackground from "@/components/animations/animated-background";
 
 export default async function DashboardLayout({
   children,
@@ -17,12 +18,24 @@ export default async function DashboardLayout({
   }
 
   return (
-    <div className="grid min-h-screen w-full lg:grid-cols-[280px_1fr]">
-      <WelcomePopup />
-      <DashboardSideBar />
-      <DashboardTopNav>
-        <main className="flex flex-col gap-4 p-4 lg:gap-6">{children}</main>
-      </DashboardTopNav>
+    <div className="relative min-h-screen w-full overflow-hidden">
+      {/* Dashboard image sab se peeche */}
+      <div className="dashboard-bg fixed inset-0 -z-20 bg-cover bg-center bg-no-repeat" />
+
+      {/* Bubbles image ke upar */}
+      <AnimatedBackground />
+
+      {/* Dashboard content sab se upar */}
+      <div className="relative z-10 grid min-h-screen w-full lg:grid-cols-[280px_1fr]">
+        <WelcomePopup />
+        <DashboardSideBar />
+
+        <DashboardTopNav>
+          <main className="flex flex-col gap-4 p-4 lg:gap-6">
+            {children}
+          </main>
+        </DashboardTopNav>
+      </div>
     </div>
   );
 }
